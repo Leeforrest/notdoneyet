0921a57473bdea20d8fa25f118722b48cc0f939e
xiaodong <1070399077@qq.com>
2017-07-04 19:08:36 +0800

    remove element

--- src/main/java/array/RemoveElement.java
+++ src/main/java/array/RemoveElement.java
@@ -5,7 +5,7 @@ package array;
  * Do not allocate extra space for another array, you must do this in place with constant memory.
  * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
  * 给定数组，给定一个值，删除数组中与给定值相等的元素，并且返回数组长度，不能创建新的数组，最终数组变成什么样你也不用关心
- * 就是说这里的需求就是要计算去掉数组中指定的元素后数组的长度是多少
+ * 就是说这里的需求就是要计算去掉数组中指定的元素后数组的长度是多少.
  * author : Forrest
  * date : 2017/7/4.
  */
